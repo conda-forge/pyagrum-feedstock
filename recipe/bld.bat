7dc6568be24cfaa00e3c93932d9986313db74821
Julien Schueller <schueller@phimeca.com>
2016-10-11 17:13:00 +0200

    Enable osx/win

---
+++ recipe/bld.bat
@@ -0,0 +1,16 @@
+
+mkdir build
+cd build
+
+set CMAKE_CONFIG="Release"
+
+cmake -LAH -G"NMake Makefiles"                               ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"                   ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"                ^
+    -DPYTHON_INSTALL="%SP_DIR%"                              ^
+    -DBUILD_SHARED_LIBS=OFF                                  ^
+    ..
+if errorlevel 1 exit 1
+
+cmake --build . --config %CMAKE_CONFIG% --target INSTALL
+if errorlevel 1 exit 1
