988211d2705bb9e9fef72c3f8529602f52e2ad2c
Julien Schueller <schueller@phimeca.com>
2017-01-03 11:48:28 +0100

    Update to 0.10.3

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,5 @@
 
-mkdir build
-cd build
+mkdir build && cd build
 
 set CMAKE_CONFIG="Release"
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 
-if test "${PY3K}" == "1"
-then
-  PY2=OFF
-else
-  PY2=ON
-fi
-
-mkdir -p build && cd build
+mkdir build && cd build
 cmake \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DFOR_PYTHON2="${PY2}" \
+  -DFOR_PYTHON3=${PY3K} \
   ..
 make install -j${CPU_COUNT}
