13466a36a43bf75c9be18c0f60024de445f07c92
Julien Schueller <schueller@phimeca.com>
2019-01-23 20:28:46 +0100

    No need for FOR_PYTHON3

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -20,11 +20,10 @@ cmake -G "MinGW Makefiles" ^
   -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX:\=/%/mingw-w64 ^
   -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX:\=/%/mingw-w64 ^
   -DPYTHON_LIBRARY=%PREFIX%/libs/libpython%CONDA_PY%.dll.a ^
-  -DCMAKE_C_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4 -DNDEBUG -DMS_WIN64" ^
-  -DCMAKE_CXX_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4 -DNDEBUG -DMS_WIN64 -D_hypot=hypot" ^
+  -DCMAKE_C_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4 -DMS_WIN64" ^
+  -DCMAKE_CXX_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4 -DMS_WIN64 -D_hypot=hypot" ^
   -DPYTHON_INSTALL="%SP_DIR:\=/%" ^
   -DBUILD_SHARED_LIBS=OFF ^
-  -DFOR_PYTHON3=%PY3K% ^
   ..
 if errorlevel 1 exit 1
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,6 @@ mkdir build && cd build
 cmake \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DFOR_PYTHON3=${PY3K} \
   ..
 make install -j${CPU_COUNT}
 ${PYTHON} ../wrappers/pyAgrum/testunits/gumTest.py
