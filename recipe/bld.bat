72681318d3b1362c1200c38bddb78741499bb32a
Julien Schueller <schueller@phimeca.com>
2019-05-10 20:19:59 +0200

    Cleanup bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,26 +7,3 @@ if errorlevel 1 exit 1
 
 %PYTHON% wrappers\pyAgrum\testunits\gumTest.py
 if errorlevel 1 exit 1
-
-exit 0
-
-: remove sh.exe from PATH
-set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
-
-mkdir build && cd build
-
-:: Configure.
-cmake -G "MinGW Makefiles" ^
-  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX:\=/%/mingw-w64 ^
-  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX:\=/%/mingw-w64 ^
-  -DPYTHON_LIBRARY=%PREFIX%/libs/libpython%CONDA_PY%.dll.a ^
-  -DCMAKE_C_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4 -DMS_WIN64" ^
-  -DCMAKE_CXX_FLAGS_RELEASE="-O2 -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4 -DMS_WIN64 -D_hypot=hypot" ^
-  -DPYTHON_INSTALL="%SP_DIR:\=/%" ^
-  -DBUILD_SHARED_LIBS=OFF ^
-  ..
-if errorlevel 1 exit 1
-
-:: Build.
-mingw32-make install -j %CPU_COUNT% VERBOSE=1
-if errorlevel 1 exit 1
