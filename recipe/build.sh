f0d4af780bdb073ab019850f7d2c7ff80cb698df
Gaspard Ducamp <ducamp.gaspard@gmail.com>
2020-03-23 22:25:24 +0100

    Add BUILD_PYTHON

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ mkdir build && cd build
 cmake \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DBUILD_PYTHON=ON \
   ..
 make install -j${CPU_COUNT}
 ${PYTHON} ../wrappers/pyAgrum/testunits/gumTest.py
