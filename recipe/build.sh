10395597cda8569c5968d3bc9e65f77909eeb42e
Gaspard Ducamp <ducamp.gaspard@gmail.com>
2022-12-05 21:08:15 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ cmake ${CMAKE_ARGS} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DBUILD_PYTHON=ON \
+  -DPython_EXECUTABLE=${PYTHON}
   ..
 make install -j${CPU_COUNT}
 if test "${BUILD}" == "${HOST}"
