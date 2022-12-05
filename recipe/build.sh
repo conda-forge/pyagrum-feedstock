024df46c3b7f05dcd42e499649190f06114aa2c9
Gaspard Ducamp <ducamp.gaspard@gmail.com>
2022-12-05 23:20:51 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+which python
+echo ${PYTHON}
+echo ${PREFIX}
+
 mkdir build && cd build
 cmake ${CMAKE_ARGS} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DBUILD_PYTHON=ON \
-  -DPython_EXECUTABLE=${PYTHON} \
   -DPython_FIND_STRATEGY=LOCATION \
   -DPython_ROOT_DIR=${PREFIX} \
   ..
