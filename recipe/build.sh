510e5cb312804fe211a07f0b53ccf004ad24e033
Julien Schueller <schueller@phimeca.com>
2022-12-06 09:05:22 +0100

    sed

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# src/cmake/CxxTest.agrum.cmake:2:find_package(Python ${PYAGRUM_REQUIRED_PYTHON_VERSION} COMPONENTS Interpreter)
+sed -i.bak "s|COMPONENTS Interpreter|COMPONENTS Interpreter Development.Module|g" src/cmake/CxxTest.agrum.cmake
+
 mkdir build && cd build
 cmake ${CMAKE_ARGS} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
@@ -7,6 +10,7 @@ cmake ${CMAKE_ARGS} \
   -DBUILD_PYTHON=ON \
   -DPython_FIND_STRATEGY=LOCATION \
   -DPython_ROOT_DIR=${PREFIX} \
+  -DZPYAGRUM_REQUIRED_PYTHON_VERSION=${PY_VER} \
   ..
 make install -j${CPU_COUNT}
 if test "${BUILD}" == "${HOST}"
