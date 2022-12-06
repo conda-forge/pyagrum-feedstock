133a1b674446049b04ae769f1600a2a10fc79dfd
Gaspard Ducamp <ducamp.gaspard@gmail.com>
2022-12-06 07:42:54 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,13 +4,15 @@ which python
 echo ${PYTHON}
 echo ${PREFIX}
 
+OURPYTHON="which python"
+echo ${OURPYTHON}
+
 mkdir build && cd build
 cmake ${CMAKE_ARGS} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DBUILD_PYTHON=ON \
-  -DPython_FIND_STRATEGY=LOCATION \
-  -DPython_ROOT_DIR=${PREFIX} \
+  -DPyhon_EXECUTABLE=${OURPYTHON} \
   ..
 make install -j${CPU_COUNT}
 if test "${BUILD}" == "${HOST}"
