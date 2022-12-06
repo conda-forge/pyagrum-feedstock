e749d20bc7899ce51595f1c6d53d54367af3c950
Gaspard Ducamp <ducamp.gaspard@gmail.com>
2022-12-06 10:34:32 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,9 @@ cmake ${CMAKE_ARGS} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DBUILD_PYTHON=ON \
+  -DPython_FIND_STRATEGY=LOCATION \
   -DPython_ROOT_DIR=${PREFIX} \
+  -DPython_INCLUDE_DIR=${PREFIX}/include/python${PY_VER} \
   ..
 make install -j${CPU_COUNT}
 if test "${BUILD}" == "${HOST}"
