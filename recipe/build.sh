6583a7b797a0bdb20ceb91fff3a9877e106ead7c
Julien Schueller <schueller@phimeca.com>
2022-12-06 08:55:02 +0100

    1.5.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,8 @@ cmake ${CMAKE_ARGS} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DBUILD_PYTHON=ON \
+  -DPython_FIND_STRATEGY=LOCATION \
+  -DPython_ROOT_DIR=${PREFIX} \
   ..
 make install -j${CPU_COUNT}
 if test "${BUILD}" == "${HOST}"
