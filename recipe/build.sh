f31aba63e38da8f551d081a8399fb0e4079045eb
Julien Schueller <schueller@phimeca.com>
2026-03-09 18:01:03 +0100

    v1

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if test `uname` == "Darwin"; then
-  export CXXFLAGS="${CXXFLAGS} -fno-assume-unique-vtables"
-fi
-
 if [[ "${target_platform}" == osx-* ]]; then
     # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
     CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
