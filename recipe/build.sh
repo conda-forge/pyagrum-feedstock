354f8386293b9c72ff5a94c769a725619467aaad
Gaspard Ducamp <ducamp.gaspard@gmail.com>
2026-07-17 17:26:04 +0200

    Modify build.sh for improved CMake Python args
    Update CMake configuration for Python integration and adjust flags for different build environments.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 
 if [[ "${target_platform}" == osx-* ]]; then
-    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
     CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+if [[ "${BUILD}" != "${HOST}" ]]; then
+    CMAKE_PYTHON_ARGS=(
+        -DPython_EXECUTABLE="${PREFIX}/bin/python"
+        -DPython_INCLUDE_DIR="${PREFIX}/include/python${PY_VER}"
+        -DPython_NumPy_INCLUDE_DIR="${PREFIX}/lib/python${PY_VER}/site-packages/numpy/core/include"
+        -DPython_FIND_STRATEGY=LOCATION
+    )
+else
+    CMAKE_PYTHON_ARGS=()
 fi
 
 cmake ${CMAKE_ARGS} \
+  "${CMAKE_PYTHON_ARGS[@]}" \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_UNITY_BUILD=ON \
   -DBUILD_PYTHON=ON \
-  -DPython_FIND_STRATEGY=LOCATION \
-  -DPython_ROOT_DIR=${PREFIX} \
   -DAGRUM_PYTHON_SABI=ON \
   -B build .
+
 cmake --build build --target install --parallel ${CPU_COUNT}
 
 if test "${BUILD}" == "${HOST}"
